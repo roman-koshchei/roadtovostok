8994ac40d55946c0af2b68fd5dfe61b14b4135d4
Roman Koshchei <romankoshchei@gmail.com>
2023-10-03 21:54:54 +0300

    preload videos thumbnails

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-
 import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch(), sitemap()],
+  integrations: [tailwind(), prefetch(), sitemap(), svelte()],
+  output: "static",
+  image: {
+    domains: ["i.ytimg.com"]
+  }
 });
---
+++ src/components/YouTube.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let id: string 
+  export let title: string 
+
+  let hidden = true
+</script>
+
+<iframe on:load={() => hidden = false} 
+  class="{hidden ? "hidden" : ""} absolute top-0 left-0 right-0 bottom-0 
+  z-10 w-full h-full"
+  width={640}
+  height={360}
+  loading="lazy"
+  src="https://www.youtube.com/embed/{id}"
+  title={title}
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  allowfullscreen
+></iframe>
