09f8e238a2ed74251db3a100f5d1d7181c2109ca
Roman Koshchei <romankoshchei@gmail.com>
2023-10-05 21:19:22 +0300

    landing page

---
+++ src/components/Screenshot.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+  export let src: string
+  let dialog: HTMLDialogElement
+</script>
+
+<button 
+  class="hover:brightness-50 transition-all cursor-zoom-in" 
+  on:click={() => dialog.showModal()}
+>
+  <slot/>
+</button>
+
+<dialog bind:this={dialog} on:click={() => dialog.close()}>
+  <img src={src} alt=""/>
+</dialog>
